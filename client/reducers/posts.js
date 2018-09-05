98d28302b0a5b43df74c06becfe227e9defb8faf
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-09-05 09:23:35 -0400

    hooking up redux

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,7 +3,11 @@
 // 1. the action (info about what happened)
 // 2. copy of current state
 
+// every reducer runs every time there is an action,
+// whether something changes in state is up to you (with switch statements)
+
 function posts(state = [], action) {
+  console.log('The post will change');
   console.log(state, action);
   return state;
 }
