98583ae6cd4fe98b16efb80197a0f1c5b36abe81
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-09-06 09:22:02 -0400

    more notes around redux

--- client/components/App.js
+++ client/components/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
 import Main from './Main';
 
+// takes whatever lives in store or state and maps them
 function mapStateToProps(state) {
   return {
     posts: state.posts,
@@ -10,13 +11,17 @@ function mapStateToProps(state) {
   };
 }
 
+// passes the actionCreators via props and dispatch to surface data
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 }
 
+// this connect needs two things: mapStateToProps (props) and mapDispatchToProps (state),
+// which are surfaced via props from the action creators
 const App = connect(
   mapStateToProps,
   mapDispatchToProps,
 )(Main);
 
+// makes it available to the other JS files that import it
 export default App;
