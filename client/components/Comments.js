e39b6f5e09857be4eb58c70abba006043cad436a
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-09-13 14:19:40 -0400

    comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -11,7 +11,16 @@ export default class Comments extends Component {
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button
+            className="remove-comment"
+            onClick={this.props.removeComment.bind(
+              null,
+              this.props.params.postId,
+              i,
+            )}
+          >
+            &times;
+          </button>
         </p>
       </div>
     );
@@ -26,12 +35,13 @@ export default class Comments extends Component {
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   }
 
   render() {
     return (
       <div className="comments">
-        {this.props.postComments.map(this.renderComment)}
+        {this.props.postComments.map(this.renderComment.bind(this))}
         <form
           ref="commentForm"
           className="comment-form"
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,33 @@
+function postComments(state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      // return the new state
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment,
+        },
+      ];
+    case 'REMOVE_COMMENT':
+      console.log('removing a comment');
+
+      return state;
+    default:
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
-  // console.log(state, action);
+  if (typeof action.postId !== undefined) {
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action),
+    };
+  }
   return state;
 }
 
