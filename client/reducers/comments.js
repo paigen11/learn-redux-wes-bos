4f1dc72c13dd854c88eabdeca6c8997cc8826d10
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-09-13 14:46:56 -0400

    comments working

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -11,8 +11,13 @@ function postComments(state = [], action) {
       ];
     case 'REMOVE_COMMENT':
       console.log('removing a comment');
-
-      return state;
+      // we need to return the new state without the deleted comment
+      return [
+        // from the start to the one we want to delete
+        ...state.slice(0, action.i),
+        // after the one we want to delete, to the end
+        ...state.slice(action.i + 1),
+      ];
     default:
       return state;
   }
